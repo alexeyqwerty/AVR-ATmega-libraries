0ce732ea27ee4ab594c7da627cee550d89f202e0
Alexey <immediant@gmail.com>
2021-08-30 22:59:58 +0300

    Add files via upload

---
+++ AD5541/AD5541.cpp
@@ -0,0 +1,24 @@
+﻿#include "AD5541.h"
+
+AD5541::AD5541(Port* CS)
+{
+	this->CS = CS;
+	
+	this->CS->SetAsOutput();
+	this->CS->Set();	
+}
+
+void AD5541::SetOutput(uint16_t data, SPI_Master* spiMaster)
+{
+	spiMaster->SetMode(FALLING, END);
+	
+	this->CS->Clear();
+	
+	spiMaster->SendByte(data >> 8);
+	spiMaster->ReceiveByte();
+	spiMaster->SendByte(data);
+	spiMaster->ReceiveByte();
+	
+	this->CS->Set();
+}
+
