ebf2b4019afa929a5ed2dc3d5a27a3bc89ba5d8b
Alexey <immediant@gmail.com>
2021-08-30 21:48:21 +0300

    Add files via upload

---
+++ StringHandler/StringHandler.cpp
@@ -0,0 +1,102 @@
+#include "StringHandler.h"
+
+const char* StringHandler::FloatToString(float value, uint8_t digitsAfterDot)
+{
+	long integerPart = fabs(value);
+	
+	uint8_t stringLength = 0;	
+	do
+	{
+		integerPart /= 10;
+		stringLength++;
+	}
+	while(integerPart > 0);
+	
+	integerPart = fabs(value);
+	
+	uint8_t integerPartLength = stringLength;
+	
+	if(digitsAfterDot > 0)
+	{
+		stringLength++;										//for '.'
+		stringLength += digitsAfterDot;						//for .xxxxx
+	}
+	
+	if(value < 0) stringLength++;							//for '-'
+	
+	stringLength++;											//for '\0'
+			
+	char tempString[stringLength];	
+			
+	long tempIntegerPart = integerPart;	
+			
+	char integerPartDigitsArray[integerPartLength + 1];		// "+ 1" for '\0'
+			
+	for(uint8_t i = 1; i <= integerPartLength; i++)
+	{
+		uint8_t dig = tempIntegerPart % 10;
+				
+		tempIntegerPart /= 10;
+				
+		integerPartDigitsArray[integerPartLength - i] = dig + '0';
+	}
+	
+	integerPartDigitsArray[integerPartLength] = '\0';
+				
+	const char* resultString = integerPartDigitsArray;
+			
+	if(value < 0)
+	{
+		strlcpy(tempString, "-", stringLength);
+		strlcat(tempString, resultString, stringLength);
+	}
+	else strlcpy(tempString, resultString, stringLength);
+	
+	if(digitsAfterDot == 0)
+	{		
+		resultString = tempString;		
+		return resultString;
+	}
+		
+	strlcat(tempString, ".", stringLength);
+		
+	value = fabs(value) - integerPart;	
+			
+	uint32_t mult = (uint32_t)round(pow(10, digitsAfterDot));
+	
+	uint32_t fractionalPart = (uint32_t)round(value * mult);
+			
+	char fractionalPartDigitsArray[digitsAfterDot + 1];		//for '\0'
+			
+	for(uint8_t i = 1; i <= digitsAfterDot; i++)
+	{
+		uint8_t dig = fractionalPart % 10;
+				
+		fractionalPart /= 10;
+				
+		fractionalPartDigitsArray[digitsAfterDot - i] = dig + '0';
+	}			
+	fractionalPartDigitsArray[digitsAfterDot] = '\0';
+	
+	resultString = fractionalPartDigitsArray;
+			
+	strlcat(tempString, resultString, stringLength);
+			
+	resultString = tempString;
+	
+	return resultString;
+}
+
+const char* StringHandler::Concat(const char* string_1, const char* string_2)
+{
+	uint16_t str_1_Length = strlen(string_1);
+	uint16_t str_2_Length = strlen(string_2);
+	uint16_t stringLength = str_1_Length + str_2_Length + 1;
+	
+	char *concatenatedString = (char*)malloc(stringLength);
+	
+	strlcpy(concatenatedString, string_1, stringLength);
+	strlcat(concatenatedString, string_2, stringLength);
+	
+	return concatenatedString;
+}
---
+++ StringHandler/StringHandler.h
@@ -0,0 +1,44 @@
+﻿/***************************************************************************
+Title:		Library for string handle
+****************************************************************************/
+
+#ifndef STRINGHANDLER_H_
+#define STRINGHANDLER_H_
+
+#include <stdint-gcc.h>
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+
+class StringHandler
+{
+	public:
+	
+	/***********************************************************************
+	Function:		FloatToString()
+	Purpose:		Convert float values to string format. Also possible to
+					use integer values with null value of the number of digits
+					after the decimal point
+	Input:			Value for converting
+					Number digits after dot
+	Returns:		Constant char pointer to a converted string
+	************************************************************************/
+	static const char* FloatToString(float value, uint8_t digitsAfterDot);	
+	
+	/***********************************************************************
+	Function:		Concat()
+	Purpose:		Combining two strings
+	Input:			First string
+					Second string
+	Returns:		Sum of two strings
+	How use:		const char* text = StringHandler::Concat(s1, s2);
+					.
+					.	...do something with text...
+					.
+					free((char*)text);
+					text = NULL;
+	************************************************************************/	
+	static const char* Concat(const char* string_1, const char* string_2);
+};
+
+#endif /* HANDLEROFNUMBERS_H_ */
