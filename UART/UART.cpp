8826f1b0af98f7945b633faa2e4d6bfc837f7e28
Alexey <immediant@gmail.com>
2022-03-24 11:41:57 +0300

    Add files via upload

---
+++ UART/UART.cpp
@@ -0,0 +1,199 @@
+#include "UART.h"
+
+const char start_mark = '*';
+const char end_mark = '$';
+
+UART::UART(uint32_t baudrate, uint32_t processorFrequency)
+{	
+	status = UART_OFF;
+	
+	TX_MessagesCount = 0;
+	TX_ReadPointer = 0;
+	TX_CharsCounter = 0;
+	TX_WritePointer = 0;
+	messageTransmissionStarted = false;	
+	
+	RX_MessagesCount = 0;
+	RX_ReadPointer = 0;
+	RX_WritePointer = 0;	
+	messageReceptionStarted = false;		
+	
+	for(uint8_t i = 0; i < maxMessages; i++)
+	{
+		TX_Messages[i].messageLength = 0;				
+		
+		RX_Messages[i].messageLength = 0;		
+		
+		for(uint8_t j = 0; j < maxWords; j++)
+		{
+			TX_Messages[i].messageText[j] = '\0';
+			RX_Messages[i].messageText[j] = '\0';
+		}				
+	}
+	
+	uint16_t ubrr_temp = (processorFrequency / (baudrate * 16)) - 1;
+		
+	#if defined(USART_ATMEGA)
+		UCSRC |= (1<<URSEL)|(1<<UCSZ1)|(1<<UCSZ0);		
+		UBRRH = (ubrr_temp>>8);
+		UBRRL = ubrr_temp;
+			
+	#elif defined(USART0_ATMEGA)
+		UCSR0C |= (1<<UCSZ01)|(1<<UCSZ00);	
+		UBRR0H = (ubrr_temp>>8);
+		UBRR0L = ubrr_temp;		
+		
+	#endif		
+}
+
+void UART::Enable()
+{
+	#if defined(USART_ATMEGA)
+		UCSRB |= (1<<RXEN)|(1<<TXEN)|(1<<RXCIE)|(1<<TXCIE);
+			
+	#elif defined(USART0_ATMEGA)
+		UCSR0B |= (1<<RXEN0)|(1<<TXEN0)|(1<<RXCIE0)|(1<<TXCIE0);	
+		
+	#endif	
+	
+	status = UART_ON;
+}
+
+void UART::Disable()
+{
+	#if defined(USART_ATMEGA)
+		UCSRB &= ~(1<<RXEN)&~(1<<TXEN)&~(1<<RXCIE)&~(1<<TXCIE);
+		
+	#elif defined(USART0_ATMEGA)
+		UCSR0B &= ~(1<<RXEN0)&~(1<<TXEN0)&~(1<<RXCIE0)&~(1<<TXCIE0);	
+		
+	#endif
+	
+	status = UART_OFF;
+}
+
+UART_Status UART::GetStatus()
+{
+	return status;
+}
+
+void UART::UploadigDataForTransfer(const char* data)
+{	
+	if(TX_MessagesCount < maxMessages)
+	{
+		uint8_t arrayLength = strlen(data);
+		
+		if(arrayLength > maxWords) return;				
+		
+		memcpy(TX_Messages[TX_WritePointer].messageText, data, arrayLength);
+		
+		TX_Messages[TX_WritePointer].messageLength = arrayLength;
+		
+		TX_WritePointer = (TX_WritePointer + 1) % maxMessages;
+		TX_MessagesCount++;	
+	}
+}
+
+void UART::TransmissionStop()
+{
+	messageTransmissionStarted = false;
+}
+
+void UART::Transmit()
+{	
+	if(messageTransmissionStarted) return;
+		
+	if(TX_MessagesCount > 0)
+	{
+		if(TX_CharsCounter == 0)
+		{
+			do 
+			{
+				TX_Message = (char*)malloc(TX_Messages[TX_ReadPointer].messageLength);
+			} while(TX_Message == NULL);
+			
+			memcpy(TX_Message, TX_Messages[TX_ReadPointer].messageText, TX_Messages[TX_ReadPointer].messageLength);
+		}
+		
+		if(TX_CharsCounter < TX_Messages[TX_ReadPointer].messageLength)
+		{
+			_UDR = TX_Message[TX_CharsCounter];			
+			
+			TX_CharsCounter++;
+			
+			messageTransmissionStarted = true;
+		}
+		
+		else
+		{
+			free(TX_Message);
+			
+			TX_CharsCounter = 0;
+			TX_ReadPointer = (TX_ReadPointer + 1) % maxMessages;
+			TX_MessagesCount--;
+		}
+	}
+}
+
+void UART::ReceiveData()
+{
+	char data = _UDR;
+
+	if((data == start_mark) && (RX_MessagesCount < maxMessages))
+	{
+		messageReceptionStarted = true;
+		
+		RX_Messages[RX_WritePointer].messageLength = 0;
+		
+		return;
+	}
+	
+	else if (data == end_mark)
+	{
+		if(messageReceptionStarted == false) return;
+	
+		RX_WritePointer = (RX_WritePointer + 1) % maxMessages;
+		RX_MessagesCount++;
+		
+		messageReceptionStarted = false;
+		return;
+	}
+
+	if(messageReceptionStarted == true)
+	{
+		RX_Messages[RX_WritePointer].messageText[RX_Messages[RX_WritePointer].messageLength] = data;
+		RX_Messages[RX_WritePointer].messageLength++;
+		
+		if(RX_Messages[RX_WritePointer].messageLength == maxWords)
+		{
+			RX_Messages[RX_WritePointer].messageLength = 0;
+			
+			messageReceptionStarted = false;
+		}
+	}
+}
+
+bool UART::HaveUnreadMessages()
+{
+	return (RX_MessagesCount > 0);	
+}
+
+const char* UART::GetRX_Message()
+{
+	do 
+	{
+		RX_Message = (char*)malloc(RX_Messages[RX_ReadPointer].messageLength);
+	} while(RX_Message == NULL);
+	
+	memcpy(RX_Message, RX_Messages[RX_ReadPointer].messageText, RX_Messages[RX_ReadPointer].messageLength);
+	
+	return RX_Message;
+}
+
+void UART::ClearRX_Message()
+{		
+	free(RX_Message);
+	
+	RX_ReadPointer = (RX_ReadPointer + 1) % maxMessages;
+	RX_MessagesCount--;
+}
---
+++ UART/UART.h
@@ -0,0 +1,215 @@
+﻿/************************************************************************************
+Title:						USART library with using RX/TX interrupts
+
+Supported microcontrollers: ATmega16, ATmega32
+							ATmega328P
+							ATmega644P, ATmega1284P(USART0)
+		 
+Notes:						- Working mode: 8 bit, no parity, 1 stop, asynchronous	
+							- 2 buffers of 10 messages for receiving and transmitting
+							- 30 characters per message	
+*************************************************************************************/
+
+#ifndef UART_H_
+#define UART_H_
+
+#include <avr/io.h>
+#include <stdlib.h>
+#include <string.h>
+
+#if defined(__AVR_ATmega16__) || defined(__AVR_ATmega32__)
+	#define USART_ATMEGA
+	#define USART_RX_INTERRUPT USART_RXC_vect
+	#define USART_TX_INTERRUPT USART_TXC_vect
+	#define _UDR UDR
+
+#elif defined(__AVR_ATmega328P__)
+	#define USART0_ATMEGA
+	#define USART_RX_INTERRUPT USART_RX_vect
+	#define USART_TX_INTERRUPT USART_TX_vect
+	#define _UDR UDR0
+
+#elif defined(__AVR_ATmega644P__) || defined(__AVR_ATmega1284P__)
+	#define USART0_ATMEGA
+	#define USART_RX_INTERRUPT USART0_RX_vect
+	#define USART_TX_INTERRUPT USART0_TX_vect
+	#define _UDR UDR0
+#endif
+
+#define maxMessages 20
+#define maxWords 30
+
+struct Message
+{
+	char messageText[maxWords];
+	uint8_t messageLength;
+};
+
+enum UART_Status
+{
+	UART_OFF,
+	UART_ON
+};
+	
+class UART
+{
+	public:		
+	
+	/***********************************************************************
+	Function:		UART()
+	Purpose:		Initializing USART
+	Input:			- Baudrate value
+					- Processor tact frequency value
+	Returns:		No
+	************************************************************************/
+	UART(uint32_t baudrate, uint32_t processorFrequency);
+	
+	/***********************************************************************
+	Functions:		Enable()
+					Disable()
+	Purpose:		Enable and disable receiver and transmitter modules 
+					and interrupts
+	Input:			No
+	Returns:		No
+	************************************************************************/
+	void Enable();	
+	void Disable();	
+	
+	/***********************************************************************
+	Function:		GetStatus()
+	Purpose:		Reporting USART active status
+	Input:			No
+	Returns:		USART active status
+	************************************************************************/
+	UART_Status GetStatus();	
+	
+	/***********************************************************************
+	Function:		UploadigDataForTransfer()
+	Purpose:		Saving messages to send in TX messages array
+	Input:			- Data to transmitting
+	Returns:		Transmission report
+	************************************************************************/	
+	void UploadigDataForTransfer(const char* data);	
+	
+	/***********************************************************************
+	Function:		TransmissionStop()
+	Purpose:		Changing transmission started flag to false, using in 
+					TX complete interrupt
+	Input:			No
+	Returns:		NO
+	************************************************************************/	
+	void TransmissionStop();
+	
+	/***********************************************************************
+	Function:		Transmit()
+	Purpose:		Transmitting first message from TX messages array and
+					deleting his after transmission. The second message for
+					transmitting becomes first
+	Input:			No
+	Returns:		No
+	************************************************************************/
+	void Transmit();
+	
+	/***********************************************************************
+	Function:		ReceiveData()
+	Purpose:		Receiving data and saving in RX_Messages array
+	Input:			No 
+	Returns:		No
+	************************************************************************/
+	void ReceiveData();	
+	
+	/***********************************************************************
+	Function:		HaveUnreadMessages()
+	Purpose:		Reporting about presence of unread messages in 
+					RX_Messages array
+	Input:			No
+	Returns:		Boolean value. True - if have unread messages
+	************************************************************************/
+	bool HaveUnreadMessages();
+	
+	/***********************************************************************
+	Function:		GetRX_Message()
+	Purpose:		Returns first incoming message from RX_Messages array
+	Input:			No
+	Returns:		Message text
+	************************************************************************/
+	const char* GetRX_Message();
+	
+	/***********************************************************************
+	Function:		ClearRX_Message()
+	Purpose:		Delete the first incoming message from RX messages array.
+					The second message becomes first.
+	Input:			No
+	Returns:		No
+	************************************************************************/
+	void ClearRX_Message();
+	
+	private:
+	
+	UART_Status status;
+	
+	struct Message TX_Messages[maxMessages];
+	char* TX_Message;
+	uint8_t TX_MessagesCount;
+	uint8_t TX_CharsCounter;
+	bool messageTransmissionStarted;
+	uint8_t TX_ReadPointer;
+	uint8_t TX_WritePointer;
+
+	struct Message RX_Messages[maxMessages];
+	char* RX_Message;
+	uint8_t RX_MessagesCount;
+	bool messageReceptionStarted;
+	uint8_t RX_ReadPointer;
+	uint8_t RX_WritePointer;
+};
+
+#endif
+
+/************************************************************************
+							HOW IT USE        							
+
+					-=Creating the UART object=-
+
+UART uart(_some_baudreit_, _some_f_cpu_);                                   
+
+*************************************************************************
+					-=Using RXC and TXC interrupts=-
+
+ISR(USART_RX_INTERRUPT)
+{
+	uart.ReceiveData();	<- Here's writing into the RX_Messages array
+}
+
+ISR(USART_TX_INTERRUPT)
+{
+	uart.TransmissionStop(); <- Here's changing transmission started flag
+}
+
+************************************************************************
+
+uart.Enable(); <- Don't forget enable UART before using
+
+sei(); <- Also global interrupts must be active
+
+************************************************************************
+					-=Insert data in TX_Messages array=-
+	
+UART_TX_Report UploadigDataForTransfer(const char* data);
+
+************************************************************************
+						-=In some cycle=-
+
+if(uart.HaveUnreadMessages()) <- Here check having unread messages
+{
+	const char* message = uart.GetRX_MessageType())	<- Here's getting the message type
+	
+	uart.ClearRX_Message(); <- Here's deleting read message. Tt's NECESSARY!!!
+}
+		
+if(!uart.TransmissionStarted()) <- The transfer should not be started
+{
+	uart.Transmit(); <- Here's transmitting data from TX_Messages array
+}
+
+************************************************************************/
