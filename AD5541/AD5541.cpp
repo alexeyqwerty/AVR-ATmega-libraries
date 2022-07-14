bcc28142eb0abda8f67bbb011c4b9a1502242bff
Alexey <immediant@gmail.com>
2022-07-14 10:38:11 +0300

    Add files via upload

---
+++ AD5541/AD5541.cpp
@@ -0,0 +1,45 @@
+﻿#include "AD5541.h"
+
+AD5541::AD5541(Port* CS)
+{
+	this->CS = CS;
+	
+	this->CS->SetAsOutput();
+	this->CS->Set();	
+}
+
+void AD5541::SetOutput(uint16_t data, SPI_Master* spiMaster)
+{
+	spiMaster->SetMode(FALLING, END);
+	
+	this->CS->Clear();
+	
+	spiMaster->SendByte(data >> 8);
+	spiMaster->ReceiveByte();
+	spiMaster->SendByte(data);
+	spiMaster->ReceiveByte();
+	
+	this->CS->Set();
+}
+
+AD5541::~AD5541()
+{
+	delete this->CS;
+}
+
+void* AD5541::operator new(size_t size)
+{
+	void* ptr;
+	do
+	{
+		ptr = malloc(size);
+	}while(ptr == nullptr);
+	
+	return ptr;
+}
+
+void AD5541::operator delete(void* ptr)
+{
+	if(ptr != nullptr) free(ptr);
+}
+
