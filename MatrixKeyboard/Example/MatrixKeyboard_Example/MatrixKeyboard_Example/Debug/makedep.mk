8188d49bc0b42c26e12082d1949e6f3577174fd2
Alexey <immediant@gmail.com>
2022-07-18 10:38:38 +0300

    Add files via upload

---
+++ MatrixKeyboard/Example/MatrixKeyboard_Example/MatrixKeyboard_Example/Debug/makedep.mk
@@ -0,0 +1,12 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+HD44780.cpp
+
+main.cpp
+
+MatrixKeyboard.cpp
+
+Port.cpp
+
