a31bb8066b9931a408d69f09806bfd597fabd2e1
Alexey <immediant@gmail.com>
2022-07-28 16:49:38 +0300

    Add files via upload

---
+++ MCP4921/MCP4921.cpp
@@ -0,0 +1,61 @@
+﻿#include "MCP4921.h"
+
+#define AB 0
+#define BUF 0
+#define GA 1
+#define SHDN 1
+
+MCP4921::MCP4921(Port *CS, Port *LDAC)
+{
+	this->CS = CS;
+	
+	this->CS->SetAsOutput();
+	this->CS->Set();
+	
+	this->LDAC = LDAC;
+		
+	this->LDAC->SetAsOutput();
+	this->LDAC->Set();	
+}
+
+void MCP4921 ::SetOutput(uint16_t data, SPI_Master* spiMaster)
+{	
+	uint8_t _data = data >> 8;
+	_data |= (AB<<7)|(BUF<<6)|(GA<<5)|(SHDN<<4);	
+
+	spiMaster->SetMode(RISING, START);
+	
+	this->CS->Clear();
+	
+	spiMaster->SendByte(_data);
+	spiMaster->ReceiveByte();	
+	
+	_data = data;
+	
+	spiMaster->SendByte(_data);		
+	spiMaster->ReceiveByte();	
+	
+	this->CS->Set();
+	
+	this->LDAC->Clear();
+	this->LDAC->Set();
+}
+
+MCP4921::~MCP4921() {}
+
+void* MCP4921::operator new(size_t size)
+{
+	void* ptr;
+	do
+	{
+		ptr = malloc(size);
+	}while(ptr == nullptr);
+	
+	return ptr;
+}
+
+void MCP4921::operator delete(void* ptr)
+{
+	free(ptr);
+}
+
---
+++ MCP4921/MCP4921.h
@@ -0,0 +1,50 @@
+﻿/************************************************************************************
+Title:						12 bit DAC MCP4921 library
+		 
+Notes:						- A pointer to SPI_Master object is used for set output
+							  voltage
+*************************************************************************************/
+
+#ifndef MCP4921_H
+#define MCP4921_H
+
+#include <avr/io.h>
+
+#include "Port.h"
+#include "SPI_Master.h"
+
+class MCP4921
+{
+	public:
+	
+	/***********************************************************************
+	Function:		MCP4921()
+	Purpose:		Initializing MCP4921
+	Input:			- Chip select port 
+					- Latch data port
+	Returns:		No
+	************************************************************************/
+	MCP4921(Port *CS, Port *LDAC);
+	
+	/***********************************************************************
+	Function:		SetOutput()
+	Purpose:		Set output voltage
+	Input:			- Output voltage code
+					- SPI master object
+	Returns:		No
+	************************************************************************/	
+	void SetOutput(uint16_t data, SPI_Master* spiMaster);	
+	
+	~MCP4921();
+	
+	void* operator new(size_t size);
+		
+	void operator delete(void* ptr);
+		
+	private:
+	
+	Port *CS;
+	Port *LDAC;
+};
+
+#endif 
