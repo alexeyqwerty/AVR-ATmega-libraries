bafeb804be310ee18eb5bc576e1fac92dbab18a4
Alexey <immediant@gmail.com>
2022-07-28 15:54:19 +0300

    Add files via upload

---
+++ CD4051/Example/CD4051_Example/CD4051_Example/CD4051.cpp
@@ -0,0 +1,51 @@
+#include "CD4051.h"
+
+CD4051::CD4051(Port* A, Port* B, Port* C, Port* INH)
+{
+	this->A = A;
+	this->B = B;
+	this->C = C;
+	this->INH = INH;	
+	
+	this->A->SetAsOutput();
+	this->B->SetAsOutput();
+	this->C->SetAsOutput();
+	this->INH->SetAsOutput();
+	this->INH->Set();
+}
+
+void CD4051::SelectChannel(uint8_t channelNumber)
+{
+	if(channelNumber > 7) return;
+	
+	true&(channelNumber) ? this->A->Set() : this->A->Clear();
+	true&(channelNumber>>1) ? this->B->Set() : this->B->Clear();
+	true&(channelNumber>>2) ? this->C->Set() : this->C->Clear();
+}
+void CD4051::EnableOutputs()
+{
+	this->INH->Clear();
+}
+
+void CD4051::DisableOutputs()
+{
+	this->INH->Set();
+}
+
+CD4051::~CD4051(){}
+
+void* CD4051::operator new(size_t size)
+{
+	void* ptr;
+	do
+	{
+		ptr = malloc(size);
+	}while(ptr == nullptr);
+	
+	return ptr;
+}
+
+void CD4051::operator delete(void* ptr)
+{
+	free(ptr);
+}
