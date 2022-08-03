f799acfa1bce58e6ebbcc76f58eab4f26d99a2b9
Alexey <immediant@gmail.com>
2022-08-03 15:56:02 +0300

    Add files via upload

---
+++ LTC1864/Example/LTC1864_Example/LTC1864_Example/Debug/LTC1864.d
@@ -0,0 +1,55 @@
+LTC1864.d LTC1864.o: .././LTC1864.cpp .././LTC1864.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
+ C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\ATmega_DFP\1.6.364\include/avr/iom8a.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h \
+ .././SPI_Master.h .././Port.h .././Devices.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\string.h \
+ .././Buffer.h
+
+.././LTC1864.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+
+C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\ATmega_DFP\1.6.364\include/avr/iom8a.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h:
+
+.././SPI_Master.h:
+
+.././Port.h:
+
+.././Devices.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\string.h:
+
+.././Buffer.h:
---
+++ LTC1864/LTC1864.cpp
@@ -0,0 +1,93 @@
+﻿#include "LTC1864.h"
+
+LTC1864::LTC1864(Port* CS)
+{
+	this->buffer = new Buffer(1);
+	
+	this->CS = CS;
+
+	this->CS->SetAsOutput();
+	this->CS->Set();
+	
+	PrepareDataChange();
+}
+
+uint16_t LTC1864::GetResult()
+{	
+	return this->result;
+}
+
+LTC1864::~LTC1864()
+{
+	delete this->buffer;
+}
+
+void* LTC1864::operator new(size_t size)
+{
+	void* ptr;
+	do
+	{
+		ptr = malloc(size);
+	}while(ptr == nullptr);
+	
+	return ptr;
+}
+
+void LTC1864::operator delete(void* ptr)
+{
+	free(ptr);
+}
+
+SPI_Polarity LTC1864::Polarity()
+{
+	return SPI_FALLING;
+}
+
+SPI_Phase LTC1864::Phase()
+{
+	return SPI_END;
+}
+
+SPI_DataOrder LTC1864::DataOrder()
+{
+	return SPI_MBF;
+}
+
+uint8_t LTC1864::DataSize()
+{
+	return this->buffer->BlockSize();
+}
+
+uint8_t * LTC1864::Data()
+{
+	return this->buffer->Read();
+}
+
+void LTC1864::Start()
+{
+	this->CS->Clear();
+}
+
+void LTC1864::Finish()
+{
+	this->CS->Set();
+		
+	uint8_t *ptr = this->buffer->Read();
+	
+	this->result = *ptr;
+	this->result <<= 8;
+		
+	ptr++;
+		
+	this->result |= *ptr;
+		
+	this->buffer->ClearBlock();
+		
+	PrepareDataChange();
+}
+
+void LTC1864::PrepareDataChange()
+{
+	uint16_t data = 0;
+	this->buffer->Write(data, sizeof(uint16_t));
+}
