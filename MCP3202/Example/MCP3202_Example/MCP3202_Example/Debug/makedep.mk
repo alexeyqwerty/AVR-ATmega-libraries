c68d4bdfb417a35b800c02fc1242411b517dbc72
Alexey <immediant@gmail.com>
2022-08-03 15:07:38 +0300

    Add files via upload

---
+++ MCP3202/Example/MCP3202_Example/MCP3202_Example/Debug/makedep.mk
@@ -0,0 +1,18 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+Buffer.cpp
+
+HD44780.cpp
+
+main.cpp
+
+MCP3202.cpp
+
+Port.cpp
+
+SPI_Master.cpp
+
+StringHandler.cpp
+
