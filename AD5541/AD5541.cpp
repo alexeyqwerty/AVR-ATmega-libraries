d54760d71543e3851c7e95fd567e7afcd098649d
Alexey <immediant@gmail.com>
2022-08-03 15:05:43 +0300

    Add files via upload

---
+++ AD5541/AD5541.cpp
@@ -0,0 +1,81 @@
+﻿#include "AD5541.h"
+
+AD5541::AD5541(Port* CS, uint8_t bufferSize)
+{
+	this->buffer = new Buffer(bufferSize);
+	
+	this->CS = CS;
+	
+	this->CS->SetAsOutput();
+	this->CS->Set();	
+}
+
+void AD5541::SetOutput(uint16_t data)
+{
+	uint8_t _data = data >> 8;
+		
+	this->buffer->Write(_data, sizeof(uint8_t));
+		
+	_data = data;
+		
+	this->buffer->Add(_data, sizeof(uint8_t));	
+}
+
+AD5541::~AD5541()
+{
+	delete this->buffer;
+}
+
+void* AD5541::operator new(size_t size)
+{
+	void* ptr;
+	do
+	{
+		ptr = malloc(size);
+	}while(ptr == nullptr);
+	
+	return ptr;
+}
+
+void AD5541::operator delete(void* ptr)
+{
+	free(ptr);
+}
+
+SPI_Polarity AD5541::Polarity()
+{
+	return SPI_FALLING;
+}
+
+SPI_Phase AD5541::Phase()
+{
+	return SPI_END;
+}
+
+SPI_DataOrder AD5541::DataOrder()
+{
+	return SPI_MBF;
+}
+
+uint8_t AD5541::DataSize()
+{
+	return this->buffer->BlockSize();
+}
+
+uint8_t * AD5541::Data()
+{
+	return this->buffer->Read();
+}
+
+void AD5541::Start()
+{
+	this->CS->Clear();
+}
+
+void AD5541::Finish()
+{
+	this->CS->Set();
+	
+	this->buffer->ClearBlock();
+}
+
---
+++ AD5541/AD5541.h
@@ -0,0 +1,68 @@
+﻿/************************************************************************************
+Title:						16 bit DAC AD5541 library
+		 
+Notes:						- A pointer to SPI_Master object is used for set output
+							  voltage
+*************************************************************************************/
+
+#ifndef AD5541_H_
+#define AD5541_H_
+
+#include <avr/io.h>
+#include <stdlib.h>
+
+#include "SPI_Master.h"
+#include "Port.h"
+#include "Buffer.h"
+
+class AD5541 : public ISPI
+{
+	public:
+	
+	/***********************************************************************
+	Function:		AD5541()
+	Purpose:		Initializing AD5541
+	Input:			- Chip select port 
+	Returns:		No
+	************************************************************************/
+	AD5541(Port* CS, uint8_t bufferSize);
+
+	/***********************************************************************
+	Function:		SetOutput()
+	Purpose:		Set output voltage
+	Input:			- Output voltage code
+					- SPI master object
+	Returns:		No
+	************************************************************************/		
+	void SetOutput(uint16_t data);
+	
+	~AD5541();
+	
+	void* operator new(size_t size);
+	
+	void operator delete(void* ptr);
+		
+	protected:
+	
+	virtual SPI_Polarity Polarity() override;
+	
+	virtual SPI_Phase Phase() override;
+
+	virtual SPI_DataOrder DataOrder() override;
+
+	virtual uint8_t DataSize() override;
+
+	virtual uint8_t * Data() override;
+
+	virtual void Start() override;
+
+	virtual void Finish() override;
+
+	private:
+		
+	Port* CS;	
+	
+	Buffer *buffer;
+};
+
+#endif /* AD5541_H_ */
